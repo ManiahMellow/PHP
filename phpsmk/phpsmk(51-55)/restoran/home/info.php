42576c51990f5b99d3de7844879e569c537b7bf1
ManiahMellow <jellakus@gmail.com>
2020-10-26 21:52:30 +0700

    PHP MINGGU 16
    Pemrograman web dan mobile ( minggu ke - 16 )

---
+++ phpsmk/phpsmk(51-55)/restoran/home/info.php
@@ -0,0 +1,4 @@
+<h4>Terimakasih, registrasi anda berhasil :), silahkan login.</h4>
+<div class="float-left mr-3">
+    <a class="btn btn-primary" href="?f=home&m=login" role="button">LOGIN</a>
+</div>
